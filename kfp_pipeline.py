3aef4e0cc8058000aa2e0abb471794ab9acde814
samyumobi <samyumobi@gmail.com>
2023-05-20 19:03:03 +0530

    docker , train, test files

--- kfp_pipeline.py
+++ kfp_pipeline.py
@@ -2,6 +2,8 @@ import kfp
 from kfp import dsl
 
 from kfp_preprocess_component import preprocess_op
+from kfp import compiler
+
 
 
 @dsl.pipeline(
@@ -11,5 +13,10 @@ from kfp_preprocess_component import preprocess_op
 def cal_pipeline():
     p = preprocess_op()
 
-client = kfp.Client()
+compiler.Compiler().compile(cal_pipeline, package_path='pipeline.yaml')
+
+client = kfp.Client(host = 'http://127.0.0.1:60029')
+client.create_run_from_pipeline_package('pipeline.yaml', arguments={'param': 'a', 'other_param': 2})
+client.create_run_from_pipeline_func('pipeline.yaml', arguments={'param': 'a', 'other_param': 2})
+
 client.create_run_from_pipeline_func(cal_pipeline,arguments={},experiment_name="my-experiment", run_name="test-run-1", namespace="kubeflow-user-example-com")
